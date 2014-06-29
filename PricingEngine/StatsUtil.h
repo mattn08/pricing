d1cc61bc788ba377ac55721f9b81ce6f8d5b1228
mattn08 <mattn08@hotmail.com>
2014-06-29 16:12:55 +0100

    Added some greeks

--- PricingEngine/StatsUtil.h
+++ PricingEngine/StatsUtil.h
@@ -2,5 +2,7 @@
 
 
 	double norm_cdf(const double& x);
-
+	double norm_pdf(const double& x);
+	double d_j(const int& j, const double& S, const double& K, const double& r,
+		const double& v, const double& T);
 
