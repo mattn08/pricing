8de2a1f6b4a567455fe0d694f4713fc2b6220bb8
matt <mattn08@hotmail.com>
2021-12-17 16:31:47 +0000

    Add another call test

--- Testing/TestOptionPrice.cpp
+++ Testing/TestOptionPrice.cpp
@@ -12,6 +12,21 @@ TEST(PriceTest, SingleOption) {
   double v = 0.2;
   double T = 1.0;
 
-  double call = call_price(100, 101, 1.01, 10, 1);
-  EXPECT_EQ(call, 10.4506);
+  double call = call_price(S, K, r, v, T);
+
+  EXPECT_FLOAT_EQ(call, 10.450576);
+}
+
+TEST(PriceTest, CallOption) {
+
+  double S = 14.58;
+  double K = 17.0;
+  double r = 0.05;
+  double v = 0.84;
+  // July 15th 2022
+  double T = 0.6;
+
+  double call = call_price(S, K, r, v, T);
+
+  EXPECT_FLOAT_EQ(call, 3.0733445);
 }
